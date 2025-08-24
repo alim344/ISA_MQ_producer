efc79c5347880829c483e036032e085f97229897
alim344 <milabudimirovic2@gmail.com>
2025-08-24 15:46:57 +0200

    Initilizing the project

---
+++ bunnycare/src/main/java/com/example/bunnycare/BunnycareApplication.java
@@ -0,0 +1,13 @@
+package com.example.bunnycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnycareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnycareApplication.class, args);
+	}
+
+}
---
+++ bunnycare/src/test/java/com/example/bunnycare/BunnycareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bunnycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnycareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
